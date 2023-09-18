66bddb2f03be90942326415fb54e1e3a4aaebee2
Slonetech <kengeregordon@gmail.com>
2023-09-18 21:34:29 +0300

    request-response-cycle

--- server/app.py
+++ server/app.py
@@ -1,10 +1,10 @@
-# server/app.py
 import os
-from flask import Flask, request, current_app,g
+
+from flask import Flask, request, current_app, g, make_response
 
 app = Flask(__name__)
 
-app.before_request
+@app.before_request
 def app_path():
     g.path = os.path.abspath(os.getcwd())
 
@@ -12,12 +12,16 @@ def app_path():
 def index():
     host = request.headers.get('Host')
     appname = current_app.name
-    return f'''<h1>The host for this page is {host}</h1>
-               <h2>The name of this application is {appname}</h2>
-               <h3>The path of this application is {g.path}</h3>'''
+    response_body = f'''
+        <h1>The host for this page is {host}</h1>
+        <h2>The name of this application is {appname}</h2>
+        <h3>The path of this application on the user's device is {g.path}</h3>
+    '''
 
-status_code = 200
-headers = {'Content-Type': 'text/html'}
-'this application only runs if the script is executed directly from the Python interpreter and not used as an imported module.'
+    status_code = 200
+    headers = {}
+
+    return make_response(response_body, status_code, headers)
+
 if __name__ == '__main__':
-    app.run(host='localhost', port=5000, debug=True)
+    app.run(port=5555)
